381d7087c99a95049deb4ac8018125624989b1f6
Barış Vural <35.barisvural@gmail.com>
2017-03-11 05:54:39 +0200

    Add files via upload

---
+++ e3s3.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:42:00 11/13/2015 
+// Design Name: 
+// Module Name:    e3s3 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module e3s3(j,k,clk,reset,q,qb
+    );
+	output q,qb;
+	reg q,qb;
+	input j,k,clk,reset;
+	wire j,k,clk,reset;
+	
+
+	always @ (posedge (clk)) begin
+		if (reset) begin
+			q <= 0;
+			qb <= 1;
+		end
+	else begin
+		if (j!=k) begin
+			q <= j;
+			qb <= k;
+		end
+  else if (j==1 && k==1) begin
+			q <= ~q;
+			qb <= ~qb;
+			end
+		end
+	end
+ 
+
+endmodule
+
+
---
+++ tb_e3s3.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:23:49 11/13/2015
+// Design Name:   e3s3
+// Module Name:   C:/Workspace/e3_s3/tb_e3s3.v
+// Project Name:  e3_s3
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: e3s3
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_e3s3;
+
+	// Inputs
+	reg j;
+	reg k;
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire q;
+	wire qb;
+
+	// Instantiate the Unit Under Test (UUT)
+	e3s3 uut (
+		.j(j), 
+		.k(k), 
+		.clk(clk), 
+		.reset(reset), 
+		.q(q), 
+		.qb(qb)
+	);
+
+	initial begin
+	$monitor("J = %b K = %b Clock = %b Reset = %b Q = %b Q/= %b" , j , k, clk ,reset,q,qb);
+  
+		j = 1;
+		k = 0;
+		clk = 1;
+		reset = 0; 
+		
+		
+			#5 j=0;
+			#5 k=1;
+			#5 k=0;
+			#5 k=1; j=1;
+			#10 $finish;
+        
+  // Add stimulus here
+ 
+	end
+		always begin 
+			#5 clk=~clk;
+		end	
+endmodule
+
